7246ec1e6832f2d073e56498915b7457d940828f
Varun <panditpautrav@gmail.com>
2026-07-14 00:59:15 +0530

    Externalize Firebase config into separate script

---
+++ firebase-config.example.js
@@ -0,0 +1,8 @@
+window.FRM_FIREBASE_CONFIG = {
+  apiKey: "PASTE_YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID"
+};
